7a358761a5caa98de880791e498eaee46b2880ad
Peter Compernolle <peter.compernolle@gusto.com>
2019-04-30 19:53:41 -0500

    fixes SubscriptionManager#detect_event_types in rspec, because rspec mocks subclass

--- lib/too_active/subscription_manager.rb
+++ lib/too_active/subscription_manager.rb
@@ -7,8 +7,11 @@ module TooActive
     private
 
     def detect_event_types
+      # note: this is probably not the ideal way to find children of Event.
+      # selecting by for_event_type helps this, and is required to avoid rspec's mocks
       ObjectSpace.each_object(Class)
         .select { |klass| klass < Event }
+        .select(&:for_event_type)
         .map { |klass| [klass.for_event_type, klass] }
         .to_h
     end
