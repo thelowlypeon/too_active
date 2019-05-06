4917c31bf078fbb7267ae6ac1088ec4d9e68b1fe
Peter Compernolle <peter.compernolle@gusto.com>
2019-05-06 08:28:55 -0500

    updates event store for analysis grouping

--- lib/too_active/event_store.rb
+++ lib/too_active/event_store.rb
@@ -2,12 +2,20 @@ module TooActive
   class EventStore
     attr_reader :events
 
-    def initialize
-      @events = []
+    def initialize(events: [])
+      @events = events
     end
 
     def <<(event)
       @events << event unless event.ignore?
+    end
+
+    def events_grouped_by_type
+      types = {}
+      events.each do |event|
+        (types[event.class] ||= []) << event
+      end
+      types
     end
   end
 end
