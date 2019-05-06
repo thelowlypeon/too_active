d9caf704a4ec220e173e631fa4a877ade62d0b9e
Peter Compernolle <peter.compernolle@gusto.com>
2019-05-06 08:54:57 -0500

    updates TooActive to include better profile method that can optionally call analyze

--- lib/too_active.rb
+++ lib/too_active.rb
@@ -1,15 +1,64 @@
 require 'too_active/event'
 Dir[File.join(__dir__, 'too_active/events/*.rb')].each { |f| require f }
+require 'too_active/analyzer'
+Dir[File.join(__dir__, 'too_active/analyzers/*.rb')].each { |f| require f }
 require 'too_active/subscription_manager'
 require 'too_active/profiler'
+require 'too_active/analysis'
+require 'too_active/formatter'
 
 module TooActive
   class << self
-    def profile(event_names = default_event_names, &block)
-      Profiler.new(build_event_types_hash(event_names), &block).profile!
+    # Profile a block with available analyzers.
+    #
+    # options:
+    #   - events:  array of event names
+    #              example: ['sql.active_record']
+    #              default: all events for which an Event class is defined
+    #   - analyze: hash of analyze options.
+    #              if false, this method will return an event store.
+    #              all other values will be passed to TooActive#analyze
+    #              default: empty hash, which will in turn use defaul #analyze options
+    #
+    # returns an event store if { analyze: false }, otherwise returns the analysis
+    def profile(opts = {}, &block)
+      event_names = opts.delete(:events) || default_event_names
+      analyze_opts = extract_analyze_opts(opts)
+
+      event_store = Profiler.new(build_event_types_hash(event_names), &block).profile!
+
+      analyze_opts.nil? ? event_store : analyze(event_store, analyze_opts)
+    end
+
+    # Analyze the events triggered while executing your block, as contained in the event store
+    #
+    # options:
+    #   - include_details: whether to include more details per event type, per analyzer
+    #                      default: true
+    #   - stdout:          whether to print the output. if false, this will return a string
+    #                      default: true
+    def analyze(event_store, opts = {})
+      include_details = opts.delete(:include_details) != false
+      to_stdout = opts.delete(:stdout) != false
+
+      analysis = Analysis.new(event_store) if event_store.events.any?
+      formatter = Formatter.new(analysis, include_details: include_details)
+      to_stdout ? formatter.print! : formatter.print
     end
 
     private
+
+    def extract_analyze_opts(opts)
+      analyze_opts = opts.delete(:analyze)
+      case analyze_opts
+      when false
+        nil
+      when Hash
+        analyze_opts
+      else
+        {}
+      end
+    end
 
     def subscription_manager
       @subscription_manager ||= SubscriptionManager.new
--- spec/too_active_spec.rb
+++ spec/too_active_spec.rb
@@ -3,10 +3,11 @@ require 'spec_helper'
 describe TooActive do
   describe '#profile' do
     let(:mock_event) { ActiveSupport::Notifications.instrument('too_active.test') }
+    let(:opts) { { analyze: false } }
 
     let(:block) { ->{ mock_event } }
 
-    subject { TooActive.profile { block.call } }
+    subject { TooActive.profile(opts) { block.call } }
 
     it 'returns an instance of event store' do
       expect(subject).to be_a TooActive::EventStore
@@ -19,7 +20,7 @@ describe TooActive do
 
     context 'when specific event types are defined' do
       let(:event_types) { ['too_active.test'] }
-      subject { TooActive.profile(event_types) { block.call } }
+      subject { TooActive.profile(events: event_types, analyze: false) { block.call } }
 
       it 'listens for that event' do
         expect(subject.events.count).to eq 1
@@ -39,6 +40,73 @@ describe TooActive do
         it 'raises' do
           expect { subject }.to raise_error(TooActive::Event::InvalidEventType, "Invalid event type 'unknown'")
         end
+      end
+    end
+
+    context 'when analyzing' do
+      let(:opts) { { analyze: true } }
+      let(:expected_analyze_options) { {} }
+      before { allow(TooActive).to receive(:analyze) }
+
+      after { subject }
+
+      shared_examples 'a correct call to analyze' do
+        it 'calls analyze with the newly constructed event_store' do
+          expect(TooActive).to receive(:analyze) do |event_store, options|
+            expect(event_store).to be_a TooActive::EventStore
+            expect(options).to eq(expected_analyze_options)
+          end
+        end
+      end
+
+      context 'when the analyze option value is true' do
+        it_behaves_like 'a correct call to analyze'
+      end
+
+      context 'when the analyzer options are a hash' do
+        let(:opts) { { analyze: { foo: :bar } } }
+        let(:expected_analyze_options) { { foo: :bar } }
+
+        it_behaves_like 'a correct call to analyze'
+      end
+    end
+  end
+
+  describe '#analyze' do
+    let(:events) do
+      [TooActive::TestEvent.mock]
+    end
+    let(:event_store) { TooActive::EventStore.new(events: events) }
+    let(:analysis) { double(TooActive::Analysis) }
+    let(:formatter) { double(TooActive::Formatter) }
+    let(:stdout) { false }
+    before do
+      allow(TooActive::Analysis).to receive(:new).with(event_store).and_return(analysis)
+      allow(TooActive::Formatter).to receive(:new).with(analysis, anything).and_return(formatter)
+    end
+
+    subject { TooActive.analyze(event_store, stdout: stdout) }
+
+    context 'when not printing to stdout' do
+      before { allow(formatter).to receive(:print) }
+
+      after { subject }
+
+      it 'creates an analysis' do
+        expect(TooActive::Analysis).to receive(:new).with(event_store)
+      end
+
+      it 'uses the analysis to create a formatter' do
+        expect(TooActive::Formatter).to receive(:new).with(analysis, include_details: true)
+      end
+    end
+
+    context 'when printing to stdout' do
+      let(:stdout) { true }
+
+      it 'prints!' do
+        expect(formatter).to receive(:print!)
+        subject
       end
     end
   end
